892f3d54f2760b9dae6db48c27fcb451fa59c8b5
t3m3d <brian@kryptonbytes.com>
2026-06-11 14:40:26 -0400

    kryofetch 1.2.1

--- Formula/kryofetch.rb
+++ Formula/kryofetch.rb
@@ -1,12 +1,12 @@
 class Kryofetch < Formula
   desc "System fetch tool for macOS, built with the Krypton language"
   homepage "https://github.com/t3m3d/kryofetch"
-  version "1.2.0"
+  version "1.2.1"
   license "MIT"
 
   on_arm do
-    url "https://github.com/t3m3d/kryofetch/releases/download/v1.2.0/kryofetch-1.2.0-macos-arm64.tar.gz"
-    sha256 "7a7a08c9a91eee779a5fc4e6a2dc81aeb6d0f7a93d012301591cfc3b625306a4"
+    url "https://github.com/t3m3d/kryofetch/releases/download/v1.2.1/kryofetch-1.2.1-macos-arm64.tar.gz"
+    sha256 "9b7fef48502a750bf28f69576d95e627f8499e715cc910e7ccbc4e1bbf52be54"
   end
 
   def install
